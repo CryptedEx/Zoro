a5b3c7990336a28c797e6846a397d99132d31503
FallenCrypt <105995404+FallenCrypt@users.noreply.github.com>
2022-05-28 16:50:37 +0100

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,8 @@
+if game.PlaceId == 286090429 then
+    print("test")
+else
+   rconsolename('Zoro V2')
+  rconsoleclear();
+  rconsoleprint('@@RED@@')
+  rconsoleprint('We have detected this game isnt on our github!\n')
+end
