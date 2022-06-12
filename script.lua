17096863f0e02bd69b5cfc9daed957c60246b792
CryptedEx <105995404+CryptedEx@users.noreply.github.com>
2022-06-12 15:34:03 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,6 +1,6 @@
 if game.PlaceId == 155615604 then
     print("test")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/FallenCrypt/Zoro/main/Games/Prison%20Life.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CryptedEx/Zoro/main/Games/Prison%20Life.lua", true))()
 else
    rconsolename('Zoro V2')
   rconsoleclear();
