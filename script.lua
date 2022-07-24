15330a6eb68977e689d7afaf2b1e625677fa5147
CryptedEx <105995404+CryptedEx@users.noreply.github.com>
2022-07-24 18:44:11 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,11 +1,10 @@
-if ZoroUser:IsInGroup(14806953) then
+--// if ZoroUser:IsInGroup(14806953) then
 if game.PlaceId == 155615604 then
-    print("test")
+    print("Prison Life")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/CryptedEx/Zoro/main/Games/Prison%20Life.lua", true))()
 else
-   rconsolename('Zoro V2')
+  rconsolename('Zoro V2')
   rconsoleclear();
   rconsoleprint('@@RED@@')
   rconsoleprint('We have detected this game isnt on our github!\n')
-end
 end
